23bc6a4079cab02ddfdd70e2d3002252ac71cbde
Fred <raychoma@hotmail.com>
2018-08-22 16:55:13 +0800

    Second change
    Refine UI and functionalities

---
+++ www/js/_variables.js
@@ -0,0 +1,150 @@
+/* THIS IS TO STORE ALL GLOBAL VARIABLES!!! */
+
+var _USER = {
+	firstName: 'John',
+	lastName: 'Doe'
+}
+
+var _PRODUCTS = [
+	{
+		id: '1',
+		title: 'Apple iPhone 8',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+	},
+	{
+		id: '2',
+		title: 'Apple iPhone 8 Plus',
+		description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+	},
+	{
+		id: '3',
+		title: 'Apple iPhone X',
+		description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+	},
+]
+
+var _ATURCARA = [
+	{
+		"tarikh": "21 September 2018",
+		"hari": "Jumaat",
+		"acara": [
+			{
+				"jam": "12.00",
+				"waktu": "pm",
+				"aktiviti": "Pembukaan kaunter pendaftaran"
+			},
+			{
+				"jam": "4.00",
+				"waktu": "pm",
+				"aktiviti": "Acara pembukaan Kem BM 2018"
+			},
+			{
+				"jam": "7.00",
+				"waktu": "pm",
+				"aktiviti": "Acara pembukaan Sabat"
+			},
+			{
+				"jam": "7.30",
+				"waktu": "pm",
+				"aktiviti": "Makan malam"
+			},
+			{
+				"jam": "9.00",
+				"waktu": "pm",
+				"aktiviti": "Acara malam Sabat"
+			},
+			{
+				"jam": "10.30",
+				"waktu": "pm",
+				"aktiviti": "Tidur!!!"
+			}
+		]
+	},
+	{
+		"tarikh": "22 September 2018",
+		"hari": "Sabtu",
+		"acara": [
+			{
+				"jam": "7.00",
+				"waktu": "am",
+				"aktiviti": "Acara pagi - persediaan rohani"
+			},
+			{
+				"jam": "8.00",
+				"waktu": "am",
+				"aktiviti": "Sarapan pagi"
+			},
+			{
+				"jam": "9.30",
+				"waktu": "am",
+				"aktiviti": "Acara Sabat"
+			},
+			{
+				"jam": "12.30",
+				"waktu": "pm",
+				"aktiviti": "Makan tengahari"
+			},
+			{
+				"jam": "2.30",
+				"waktu": "pm",
+				"aktiviti": "Acara kerohanian petang"
+			},
+			{
+				"jam": "6.00",
+				"waktu": "pm",
+				"aktiviti": "Acara penutupan Sabat"
+			},
+			{
+				"jam": "7.30",
+				"waktu": "pm",
+				"aktiviti": "Makan malam"
+			},
+			{
+				"jam": "9.00",
+				"waktu": "pm",
+				"aktiviti": "Acara kerohanian malam"
+			},{
+				"jam": "11.30",
+				"waktu": "pm",
+				"aktiviti": "Tidur!!!"
+			}
+		]
+	},
+	{
+		"tarikh": "23 September 2018",
+		"hari": "Ahad",
+		"acara": [
+			{
+				"jam": "7.00",
+				"waktu": "am",
+				"aktiviti": "Acara kerohanian pagi"
+			},
+			{
+				"jam": "8.30",
+				"waktu": "am",
+				"aktiviti": "Sarapan pagi"
+			},
+			{
+				"jam": "10.30",
+				"waktu": "am",
+				"aktiviti": "Acara penutupan Kem BM 2018"
+			},
+			{
+				"jam": "12.30",
+				"waktu": "pm",
+				"aktiviti": "Makan tengahari"
+			},
+			{
+				"jam": "2.00",
+				"waktu": "pm",
+				"aktiviti": "Selesai!!!"
+			},
+			{
+				"jam": "6.00",
+				"waktu": "pm",
+				"aktiviti": "Acara penutupan Sabat"
+			}
+		]
+	}
+
+]
--- www/js/app.js
+++ www/js/app.js
@@ -1,69 +1,136 @@
+
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/* INIT */
+
 // Dom7
 var $$ = Dom7;
 
+// Templates
+var tmp_aturcara = $$('script#aturcara').html();
+
 // Framework7 App main instance
 var app  = new Framework7({
-  root: '#app', // App root element
-  id: 'io.framework7.testapp', // App bundle ID
-  name: 'Framework7', // App name
-  theme: 'auto', // Automatic theme detection
-  // App root data
-  data: function () {
-    return {
-      user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
-      // Demo products for Catalog section
-      products: [
-        {
-          id: '1',
-          title: 'Apple iPhone 8',
-          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
-        },
-        {
-          id: '2',
-          title: 'Apple iPhone 8 Plus',
-          description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
-        },
-        {
-          id: '3',
-          title: 'Apple iPhone X',
-          description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
-        },
-      ]
-    };
-  },
-  // App root methods
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
-    },
-  },
-  // App routes
-  routes: routes,
+	root: '#app', // App root element
+	id: 'io.framework7.testapp', // App bundle ID
+	name: 'Framework7', // App name
+	theme: 'auto', // Automatic theme detection
+	// App root data
+	data: function () {
+		return {
+			user: _USER,
+			// Demo products for Catalog section
+			products: _PRODUCTS,
+			aturcara: _ATURCARA
+		};
+	},
+	// App root methods
+	methods: {
+		helloWorld: function () {
+			app.dialog.alert('Hello World!');
+		},
+	},
+	// App routes
+	routes: routes,
+	on: {
+		pageInit: init
+	}
 });
 
 // Init/Create views
 var homeView = app.views.create('#view-home', {
-  url: '/'
-});
-var catalogView = app.views.create('#view-catalog', {
-  url: '/catalog/'
-});
-var settingsView = app.views.create('#view-settings', {
-  url: '/settings/'
+	url: '/'
 });
 
+var catalogView = app.views.create('#view-catalog', {
+	url: '/catalog/'
+});
+
+var settingsView = app.views.create('#view-settings', {
+	url: '/settings/'
+});
+
+
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/* EVENTS HANDLING */
 
 // Login Screen Demo
 $$('#my-login-screen .login-button').on('click', function () {
-  var username = $$('#my-login-screen [name="username"]').val();
-  var password = $$('#my-login-screen [name="password"]').val();
+	var name = $$('#my-login-screen [name="name"]').val();
+	var email = $$('#my-login-screen [name="email"]').val();
+	var telephone = $$('#my-login-screen [name="telephone"]').val();
 
-  // Close login screen
-  app.loginScreen.close('#my-login-screen');
+	// Save the info into the formdata
+	app.form.storeFormData('form-userinfo', {
+		'name': name,
+		'email': email,
+		'telephone': telephone
+	});
+		
+	closeLoginPage();
 
-  // Alert username and password
-  app.dialog.alert('Username: ' + username + '<br>Password: ' + password);
+	// Alert username and password
+	app.dialog.alert('Name: ' + name + '<br>Email: ' + email + '<br>Telephone No: ' + telephone);
 });
+
+
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/*********************************************************************/
+/* SELF DEFINED METHODS */
+
+function init(){
+	initiateTemplates();
+	initiateLoginPage();
+}
+
+function initiateTemplates(){
+	// For aturcara
+	var aturcaraTemplate = Template7.compile(tmp_aturcara),
+		context = { aturcara : _ATURCARA };
+	aturcaraTemplate(context);
+}
+
+function initiateLoginPage(){
+	//prepareApp();
+
+	var user = app.form.getFormData('form-userinfo');
+	if (_.isEmpty(user)){
+		openLoginPage();
+	}
+}
+
+function openLoginPage(){
+	app.loginScreen.open('#my-login-screen');
+}
+
+function closeLoginPage(){
+	app.loginScreen.close('#my-login-screen');
+	$$('.page-content').scrollTop(0);
+}
+
+function prepareApp(){
+	// This part is to standy the list of calls and page generation
+	// that needs to be completed upon opening the app.
+
+	app.request.get('mocks/aturcara.json', function(data){
+		console.log("Can i see what is ::: ", data);
+		prepareCampSchedule(data);
+	});
+}
+
+function prepareCampSchedule(){
+	app.request.get('mocks/aturcara.json', function(data){
+		console.log("Can i see what is ::: ", data);
+		prepareCampSchedule(data);
+	});
+}	
